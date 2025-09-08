95865a36a75f230fadb303855f5259ccbd68cc68
vanessa-laii <vanessa.sw.lai@gmail.com>
2025-09-07 20:40:24 -0400

    new graphic

--- app/(home)/components/Graphic.tsx
+++ app/(home)/components/Graphic.tsx
@@ -1,57 +1,8 @@
-import Image from 'next/image'
-import { useState } from 'react'
- 
-function Home() {
-  const [isHovered, setIsHovered] = useState(false)
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
+import React from "react";
+import Image from "next/image";
 
-  const handleMouseMove = (e: React.MouseEvent) => {
-    const rect = e.currentTarget.getBoundingClientRect()
-    setMousePosition({
-      x: e.clientX - rect.left,
-      y: e.clientY - rect.top
-    })
-  }
-
+export function Graphic() {
   return (
-    <div className="relative flex items-center justify-center">
-      {/* Center the green circle behind the image */}
-      <div className="absolute left-1/2 top-1/2 w-[600px] h-[600px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-green-400 opacity-20 blur-[100px] z-[-1]"></div>
-      
-      <div 
-        className="relative"
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-        onMouseMove={handleMouseMove}
-      >
-        <Image
-          src="/heroGraphic.png"
-          alt="Landscape picture"
-          height={500}
-          width={500}
-        />
-        
-        {/* Speech Bubble that follows mouse */}
-        {isHovered && (
-          <div 
-            className="absolute z-10 pointer-events-none transition-all duration-150 ease-out"
-            style={{
-              left: mousePosition.x,
-              top: mousePosition.y - 60,
-              transform: 'translateX(-50%)'
-            }}
-          >
-            <div className="relative bg-white px-4 py-2 rounded-lg shadow-lg border border-gray-200">
-              <p className="text-gray-900 font-medium whitespace-nowrap">Thanks for visiting! 🫶🏻 </p>
-              {/* Speech bubble tail */}
-              <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-white"></div>
-              <div className="absolute top-full left-1/2 transform -translate-x-1/2 translate-y-[-1px] w-0 h-0 border-l-8 border-r-8 border-t-8 border-l-transparent border-r-transparent border-t-gray-200"></div>
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
-  )
+    <Image src="/polaroids.png" alt="" width={700} height={700} />
+  );
 }
- 
-export default Home
--- app/(home)/components/Hero.tsx
+++ app/(home)/components/Hero.tsx
@@ -1,16 +1,16 @@
 import Link from 'next/link';
 import React from 'react';
-import Graphic from './Graphic';
+import { Graphic } from './Graphic';
 import Title from './Title';
 import { MovingBorderBtn } from '@/components/ui/moving-border';
 import Typewriter from '@/components/ui/typewriter';
 
 export default function Hero() {
   return (
-    <div className="min-h-[60vh] flex flex-col-reverse lg:flex-row items-center justify-between p-5 gap-20 animate-move-up">
+    <div className="min-h-[60vh] flex flex-col lg:flex-row items-center justify-between p-5 gap-7 animate-move-up">
       {/* Left Section */}
       <div className="space-y-8 text-left lg:w-3/5">
-        <Typewriter className="text-3xl lg:text-6xl font-bold block underline underline-offset-8 decoration-purple-200"
+        <Typewriter className="text-5xl lg:text-6xl font-bold block underline underline-offset-8 decoration-purple-200"
           text = "Hi, I’m Vanessa Lai."
         />
 
@@ -21,13 +21,21 @@ export default function Hero() {
           <br />
           𖥔 Passionate about creating seamless digital experiences through code and creativity.
         </p>
+        
 
-        <div className="flex gap-20 items-center">
-          <div className="relative w-[275px] h-[100px] transform rotate-[-5deg] shadow-lg">
+        <div className="flex gap-5 items-center">
+          <div className="relative w-[275px] h-[150px] transform rotate-[-9deg] shadow-lg">
             <div className="absolute inset-0 bg-[#cafcb8bb] rounded-md p-4">
               <div className="h-full flex flex-col justify-between">
                 <div>
-                  <h3 className="text-xl font-medium">Navigating the world of code and design. 『••✎••』</h3>
+                  <h3 className="text-xl font-medium">Navigating the world of code and design. 『••✎••』 <br/><br/>
+                    <Link
+                      href={"https://drive.google.com/file/d/1u5rZD-lbzy0M4azHsZtnum2_2_ZMNMXH/view?usp=sharing"}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className = "font-thin underline decoration-purple-200 text-md"
+                    >Click here for my Resume</Link>
+                  </h3>
                 </div>
               </div>
             </div>
@@ -42,29 +50,19 @@ export default function Hero() {
               </div>
             </div>
           </div>
+          
         </div>
 
       </div>
 
-      {/* Vertical Divider */}
-      <div className="block w-px h-[60%] bg-gray-300 mx-6" />
 
       {/* Right Section */}
       <div className="relative flex justify-center items-center w-full lg:w-2/5">
-        <div className="space-y-3 right-0 relative flex justify-left items-left w-full lg:w-auto">
+        <div className="relative space-y-3 flex w-full lg:w-auto">
           <Graphic />
         </div>
-        <div className="absolute right-5 top-20">
-          <Link
-            href={"https://drive.google.com/file/d/1u5rZD-lbzy0M4azHsZtnum2_2_ZMNMXH/view?usp=sharing"}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <MovingBorderBtn borderRadius="0.5rem" className="p-3 font-semibold">
-              <p>⊹ My Resume ⊹</p>
-            </MovingBorderBtn>
-          </Link>
-        </div>
+        
+
       </div>
       <div className="absolute -z-10 inset-0 h-full w-full 
         bg-[linear-gradient(to_right,#73737320_1px,transparent_1px),linear-gradient(to_bottom,#73737320_1px,transparent_1px)] 
@@ -73,4 +71,8 @@ export default function Hero() {
     </div>
   );
 }
+
+
+
+
 
